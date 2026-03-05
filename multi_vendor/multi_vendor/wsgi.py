108f951b987e8ad1afe8b68a9fbfc3a6dad760e7
Ibrahim-sys-ux <ibrahimkutty.c3.11@gmail.com>
2026-03-05 15:10:25 +0530

    Update wsgi.py

--- multi_vendor/multi_vendor/wsgi.py
+++ multi_vendor/multi_vendor/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'multi_vendor.settings')
 
 application = get_wsgi_application()
+app = application
