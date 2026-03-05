2a0c6bcb93dc1bdddbd8d76e751d116e34ee80a8
Ibrahim-sys-ux <ibrahimkutty.c3.11@gmail.com>
2026-03-05 15:16:31 +0530

    Remove redundant app variable assignment

--- multi_vendor/multi_vendor/wsgi.py
+++ multi_vendor/multi_vendor/wsgi.py
@@ -14,4 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'multi_vendor.settings')
 
 application = get_wsgi_application()
-app = application
